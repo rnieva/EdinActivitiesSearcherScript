722a70e14656990fe4b2fa767f386a0512ea143c
rnieva <roberto.nieva@gmail.com>
2016-09-07 12:51:01 +0100

    Get result of first page

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Text.RegularExpressions;
+using System.Collections;
 
 namespace ActivitiEdinSearchScript
 {
@@ -17,21 +18,102 @@ namespace ActivitiEdinSearchScript
             string url2 = "https://www.joininedinburgh.org/?q=english&at=46&ns="; // Keyword = english,
             var client = new WebClient();
             string webContent = client.DownloadString(url2);
+            //get WebContent
             // Regular Expression
-            String h1Regex = "<h1[^>]*?>(?<TagText>.*?)</h1>";
+            ShowNumberOfResults(webContent);
+            string olContent = "";
+            olContent = GetContentResultsOl(webContent, olContent);
+            ArrayList listMatchesDiv = new ArrayList();
+            listMatchesDiv = GetContentResultsDiv(olContent, listMatchesDiv);
+            ArrayList listLinks = new ArrayList();
+            listLinks = GetLinksMoreResuts(listMatchesDiv, listLinks);
+            ArrayList listItems = new ArrayList();
+            listItems = GetListMatchesClear(listMatchesDiv, listItems);
+
+            if (listLinks != null)
+            {
+                url2 = url2 + listLinks[1];
+            }
+
+            
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+
+        static void ShowNumberOfResults(string webContent)
+        {
+            string h1Regex = "<h1[^>]*?>(?<TagText>.*?)</h1>";
             MatchCollection mc = Regex.Matches(webContent, h1Regex, RegexOptions.Singleline);
             foreach (Match m in mc)
             {
                 Console.WriteLine(m.Groups["TagText"].Value);
             }
-            
-
-            //Console.WriteLine(mc);
-            Console.ReadKey();
         }
-       
 
-       
+        static string GetContentResultsOl(string webContent, string olContent)
+        {
+            String olRegex = "<ol class=\"search-results\">(?<content>.*)<div id=\"column\">"; //read from class="search-result" until id="column", the final the second ol
+            MatchCollection mc2 = Regex.Matches(webContent, olRegex, RegexOptions.Singleline);
+            foreach (Match m in mc2)
+            {
+                olContent = (m.Groups["content"].Value);
+            }
+            return olContent;
+        }
+
+        static ArrayList GetContentResultsDiv(string olContent, ArrayList listMatchesDiv)
+        {
+            String divRegex = "<div[^>]*?>(.*?)</div>"; // content within the divs
+            MatchCollection mc3 = Regex.Matches(olContent, divRegex, RegexOptions.Singleline);
+
+            foreach (Match m in mc3)
+            {
+                listMatchesDiv.Add(m.Groups[1].Value);
+            }
+            return listMatchesDiv;
+        }
+
+        static ArrayList GetLinksMoreResuts(ArrayList listMatchesDiv, ArrayList listLinks)
+        {
+            string href = "href\\s*=\\s*(?:[\"'](?<1>[^\"']*)[\"']|(?<1>\\S+))";
+            MatchCollection moreResults = Regex.Matches(listMatchesDiv[listMatchesDiv.Count - 1].ToString(), href, RegexOptions.Singleline);
+            foreach (Match m in moreResults)
+            {
+                listLinks.Add(m.Groups[1].Value);
+            }
+            return listLinks;
+        }
+
+        static ArrayList GetListMatchesClear(ArrayList listMatchesDiv, ArrayList listItems)
+        {
+            ArrayList listMatchesClear = new ArrayList();
+            String itemRegex = "<p[^>]*?>(.*?)</p>";
+            String itemRegex2 = "<ul[^>]*?>(.*?)</ul>";
+            int i = 0;
+            for (i = 0; i < listMatchesDiv.Count; i++)
+            {
+                MatchCollection mc4 = Regex.Matches(listMatchesDiv[i].ToString(), itemRegex, RegexOptions.Singleline);
+                foreach (Match m in mc4)
+                {
+                    listMatchesClear.Add(m.Groups[1].Value);
+                }
+
+                MatchCollection mc5 = Regex.Matches(listMatchesDiv[i].ToString(), itemRegex2, RegexOptions.Singleline);
+                foreach (Match m in mc5)
+                {
+                    listMatchesClear.Add(m.Groups[1].Value);
+                    listMatchesClear.Add("ENDITEM");
+                }
+
+            }
+            i = 0;
+            for (i = 0; i < listMatchesClear.Count; i++)
+            {
+                string result = (Regex.Replace(listMatchesClear[i].ToString(), @"<[^>]*>", String.Empty));
+                listItems.Add(Regex.Replace(result, @"\t|\n|\r", " "));
+            }
+            return listItems;
+        }
 
     }
 }
