5eb74ef02f4c029ae0d5a4258bc1114cf607ef85
Javier Garcia <javier.garciaramos@almondnetworks.org.uk>
2016-10-17 19:59:58 +0100

    Added two Unit Tests

--- EdinActivityUnitTest/ProgramUnitTests.cs
+++ EdinActivityUnitTest/ProgramUnitTests.cs
@@ -21,5 +21,35 @@ namespace EdinActivityUnitTest
             string expected = "";
             Assert.AreEqual(expected, result);
         }
+
+        [TestMethod]
+        public void givenUrlWithOnlyOneParameter_thenReturnAStringWithTheParameter()
+        {
+            //Arrange
+            string url = "https://www.joininedinburgh.org/?q=english&at=46&a=&distance=&pc=&location=&ds_month_year=&de_month_year=";
+
+            //Act
+            string result = Program.GetNumberOfKeyWords(url);
+
+            //Assert--
+            string expected = "english - ";
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void givenDifferentUrlWithAparameter_thenReturnAStringWithTheParameterNextToDash()
+        {
+            //Arrange
+            string url = "https://www.google.es/?gws_rd=ssl#q=guion+en+ingles";
+
+            //Act
+            string result = Program.GetNumberOfKeyWords(url);
+
+            //Assert--
+            string expected = "ssl#q=guion+en+ingles - ";
+            Assert.AreEqual(expected, result);
+        }
+        //Duda: En vez de hacer diferentes UT para diferentes URLs, probar todas las URLs en el mismo UT con diferentes Asserts
+
     }
 }
